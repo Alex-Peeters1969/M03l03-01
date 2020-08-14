d5f68853d7853eadef8918ea855e4fc52c66cd51
Alex-Peeters1969 <67190405+Alex-Peeters1969@users.noreply.github.com>
2020-08-14 14:19:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let name='Alex';    
+
+console.log('Alex');
+
+var x= 4 + 4;
+
+console.log('8');
+
+let same='4' +'4';
+
+console.log('8');
+
+let age= 48;
+typeof age
+console.log("number");
+
+let year='forty-eight';
+typeof year
+console.log("string");
